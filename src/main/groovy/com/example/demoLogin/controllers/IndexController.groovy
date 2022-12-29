59938df483f801c68d7ef64af2d38b3865aac666
Jovany Vergara <jovany@makingdevs.com>
2022-12-29 00:31:57 -0600

    [1] add files to ignore

--- src/main/groovy/com/example/demoLogin/controllers/IndexController.groovy
+++ src/main/groovy/com/example/demoLogin/controllers/IndexController.groovy
@@ -1,14 +1,16 @@
-package com.example.demoLogin.Controllers
+package com.example.demoLogin.controllers
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
 class IndexController {
 
 	@GetMapping("/")
-	public String index() {
-		return "home";
+	ModelAndView index() {
+    ModelAndView model = new ModelAndView("home");
+		return model;
 	}
 
 }
