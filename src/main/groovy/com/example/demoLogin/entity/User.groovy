0f7acae56ea55c3df989f5debe91b19193132a30
Jovany Vergara <jovany@makingdevs.com>
2022-12-30 14:16:19 -0600

    [1] add entity of user

---
+++ src/main/groovy/com/example/demoLogin/entity/User.groovy
@@ -0,0 +1,19 @@
+package com.example.demoLogin.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.SequenceGenerator
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = 'USERS')
+class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Integer id
+  String name
+  String password
+}
