8ee0afd7584192827049f44612f3cf1df2515f99
Jovany Vergara <jovany@makingdevs.com>
2023-01-03 10:37:17 -0600

    [1] create entity

--- src/main/groovy/com/example/demoLogin/entity/User.groovy
+++ src/main/groovy/com/example/demoLogin/entity/User.groovy
@@ -2,6 +2,7 @@ package com.example.demoLogin.entity
 
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
 
