d9d2b4027dc2988dc9f6ca77c3a362b6575fd8d2
Jovany Vergara <jovany@makingdevs.com>
2023-01-03 10:36:53 -0600

    [1] create repository

--- src/main/groovy/com/example/demoLogin/repository/UserRepository.groovy
+++ src/main/groovy/com/example/demoLogin/repository/UserRepository.groovy
@@ -1,1 +1,14 @@
 package com.example.demoLogin.repository
+
+import com.example.demoLogin.entity.User
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.domain.Sort
+
+@Repository
+interface UserRepository extends CrudRepository<User, Integer> {
+
+  List<User> findAll()
+
+}
