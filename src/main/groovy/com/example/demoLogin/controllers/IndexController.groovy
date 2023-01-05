20aa02cd99918c067c127e0b9d9bb03d77abf9fd
Jovany Vergara <jovany@makingdevs.com>
2023-01-05 10:22:11 -0600

    [] inport messages

--- src/main/groovy/com/example/demoLogin/controllers/IndexController.groovy
+++ src/main/groovy/com/example/demoLogin/controllers/IndexController.groovy
@@ -3,6 +3,8 @@ package com.example.demoLogin.controllers
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class IndexController {
@@ -18,5 +20,13 @@ class IndexController {
     ModelAndView model = new ModelAndView("register");
 		return model;
 	}
+
+	@PostMapping("/testPost")
+    @ResponseBody
+    String testPost() {
+
+        return "has hecho una peticion post";
+
+    }
 
 }
