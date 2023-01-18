bfe041e8bdfd561d5a90ac4c02d3a9eaac3fafee
Jovany Vergara <jovany@makingdevs.com>
2023-01-18 11:48:39 -0600

    [4] add congiguration in vcConfig.

--- src/main/groovy/com/example/demoLogin/config/MvcConfig.groovy
+++ src/main/groovy/com/example/demoLogin/config/MvcConfig.groovy
@@ -1,10 +1,10 @@
-//package com.example.demoLogin.config
+package com.example.demoLogin.config
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
+//@Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
    @Override
    public void addViewControllers(ViewControllerRegistry registry) {
