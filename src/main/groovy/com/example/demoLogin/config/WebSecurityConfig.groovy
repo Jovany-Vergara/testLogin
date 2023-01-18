654746da8c9f6cedcdc00d590d3d67aba579e632
Jovany Vergara <jovany@makingdevs.com>
2023-01-18 12:34:41 -0600

    [4] add congiguration in WebSecurityConfig

--- src/main/groovy/com/example/demoLogin/config/WebSecurityConfig.groovy
+++ src/main/groovy/com/example/demoLogin/config/WebSecurityConfig.groovy
@@ -1,33 +1,47 @@
-//package com.example.demoLogin.config
+package com.example.demoLogin.config
 
 import org.springframework.beans.factory.annotation.Autowired;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.context.annotation.Bean
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-   @Override
-   protected void configure(HttpSecurity http) throws Exception {
-      http
-         .authorizeRequests()
-            .antMatchers("/", "/home").permitAll()
-            .anyRequest().authenticated()
-            .and()
-         .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-            .logout()
-            .permitAll();
-   }
-   @Autowired
-   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-      auth
-         .inMemoryAuthentication()
-         .withUser("user").password("password").roles("USER");
-   }
+    @Bean(name = "myPasswordEncoder")
+    public PasswordEncoder getPasswordEncoder() {
+        DelegatingPasswordEncoder delPasswordEncoder = (DelegatingPasswordEncoder) PasswordEncoderFactories
+                .createDelegatingPasswordEncoder();
+        BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
+        delPasswordEncoder.setDefaultPasswordEncoderForMatches(bcryptPasswordEncoder);
+        return delPasswordEncoder;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers("/index.html")
+                .hasAnyRole("USER","ADMIN")
+                .antMatchers("/admin/admin.html")
+                .hasRole("ADMIN")
+                .and()
+                .formLogin()
+
+                .and()
+                .logout();
+
+
+    }
 }
